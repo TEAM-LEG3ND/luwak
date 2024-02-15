d3240d46dce77b953993cd5b11112378c75105ae
ammerss <amyss0907@gmail.com>
2024-02-14 21:59:44 -0500

    custom user repository

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { UserRepository } from './user.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UserRepository],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,50 +1,29 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { User } from './user.entity';
 import { CreateUserDto } from './dto/user.dto';
+import { UserRepository } from './user.repository';
 
 @Injectable()
 export class UsersService {
-  constructor(
-    @InjectRepository(User)
-    private usersRepository: Repository<User>,
-  ) {}
+  constructor(private usersRepository: UserRepository) {}
 
   async getAllUsers() {
-    const users = this.usersRepository.find();
-    return users;
+    return await this.usersRepository.getAllUsers();
   }
   async getUserById(id: number) {
-    const user = await this.usersRepository.findOne({
-      where: {
-        id: id,
-      },
-    });
+    const user = await this.usersRepository.findById(id);
     if (user) {
       return user;
     }
     throw new NotFoundException('Could not find the user');
   }
+
   async createUser(createUserDto: CreateUserDto) {
-    const newUser = await this.usersRepository.create(createUserDto);
-    await this.usersRepository.save({
-      name: createUserDto.name,
-      email: createUserDto.email,
-      password: createUserDto.password,
-    });
-    return newUser;
+    return await this.usersRepository.createUser(createUserDto);
   }
+
   async deleteById(id: number) {
-    const user = await this.usersRepository.findOne({
-      where: {
-        id: id,
-      },
-    });
-    if (!user) {
-      return null;
-    }
-    await this.usersRepository.remove(user);
-    return user;
+    const user = await this.usersRepository.deleteById(id);
+    if (user) return user;
+    throw new NotFoundException('Could not find the user');
   }
 }
