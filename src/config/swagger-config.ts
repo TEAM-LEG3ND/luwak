b7ad0115c877e58ca48fd51200149b7e1b753e3f
selgyun <kindkiz@naver.com>
2024-02-16 14:08:26 +0900

    style: npm run format

--- src/config/swagger-config.ts
+++ src/config/swagger-config.ts
@@ -4,13 +4,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 export const ACCESS_TOKEN = 'access-token';
 
 export function initSwaggerConfig(app: INestApplication): void {
-    const config = new DocumentBuilder()
-        .setTitle('ODC api docs')
-        .setDescription('Team Leg3nd server')
-        .setVersion('1.0.0')
-        .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'Token'}, ACCESS_TOKEN)
-        .build();
+  const config = new DocumentBuilder()
+    .setTitle('ODC api docs')
+    .setDescription('Team Leg3nd server')
+    .setVersion('1.0.0')
+    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'Token' }, ACCESS_TOKEN)
+    .build();
 
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api-docs', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 }
