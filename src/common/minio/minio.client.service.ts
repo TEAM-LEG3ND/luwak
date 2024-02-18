1b36941520d610c4079a1453564dd4e837d41b44
redjen8 <seokwoo2525@gmail.com>
2024-02-18 11:34:40 +0900

    Feat: upload 메서드 작성

--- src/common/minio/minio.client.service.ts
+++ src/common/minio/minio.client.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { MinioService } from 'nestjs-minio-client';
 import { BufferedFile } from './model/file.model';
 import { MinioUploadResult } from './model/upload.model';
+import * as crypto from 'crypto';
 
 @Injectable()
 export class MinioClientService {
@@ -9,7 +10,29 @@ export class MinioClientService {
   constructor(private readonly minio: MinioService) {}
 
   public async upload(file: BufferedFile, targetBucket: string = this.DEFAULT_BUCKET): Promise<MinioUploadResult> {
-    return new MinioUploadResult();
+    if (!(file.mimetype.includes('jpeg') || file.mimetype.includes('png'))) {
+      throw new HttpException('Error uploading file', HttpStatus.BAD_REQUEST);
+    }
+    let temp_filename = Date.now().toString();
+    let hashedFileName = crypto.createHash('md5').update(temp_filename).digest('hex');
+    let ext = file.originalname.substring(file.originalname.lastIndexOf('.'), file.originalname.length);
+    const metaData = {
+      'Content-Type': file.mimetype,
+    };
+    let filename = hashedFileName + ext;
+    const fileName: string = `${filename}`;
+    const fileBuffer = file.buffer;
+    this.client.putObject(targetBucket, fileName, fileBuffer, function (err, res) {
+      if (err) throw new HttpException('Error uploading file', HttpStatus.BAD_REQUEST);
+    });
+
+    return {
+      url: `${process.env.MINIO_ENDPOINT}:${process.env.MINIO_PORT}/${targetBucket}/${filename}`,
+    };
+  }
+
+  private get client() {
+    return this.minio.client;
   }
 
   public async delete(): Promise<void> {
