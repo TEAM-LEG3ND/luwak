7d28fe362433cc794d0b3cbc882c8f56243913a3
selgyun <kindkiz@naver.com>
2024-02-22 12:22:28 +0900

    feat: Add findByPaymentKey API

--- src/payment/Dto/payment.dto.ts
+++ src/payment/Dto/payment.dto.ts
@@ -1,6 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { CancelDto } from './cancel.dto';
 import { PaymentStatus, PaymentType } from '../payment.enum';
+import { Payment } from '../payment.entity';
 
 export class PaymentDto {
   @ApiProperty()
@@ -32,4 +33,43 @@ export class PaymentDto {
 
   @ApiProperty()
   status: PaymentStatus;
+
+  constructor(
+    id: number,
+    paymentKey: string,
+    totalAmount: number,
+    balanceAmount: number,
+    type: PaymentType,
+    orderId: string,
+    orderName: string,
+    mId: string,
+    cancels: CancelDto[],
+    status: PaymentStatus,
+  ) {
+    this.id = id;
+    this.paymentKey = paymentKey;
+    this.totalAmount = totalAmount;
+    this.balanceAmount = balanceAmount;
+    this.type = type;
+    this.orderId = orderId;
+    this.orderName = orderName;
+    this.mId = mId;
+    this.cancels = cancels;
+    this.status = status;
+  }
+
+  static fromPayment(payment: Payment) {
+    return new PaymentDto(
+      payment.id,
+      payment.paymentKey,
+      payment.totalAmount,
+      payment.balanceAmount,
+      payment.type,
+      payment.orderId,
+      payment.orderName,
+      payment.mId,
+      payment.cancels,
+      payment.status,
+    );
+  }
 }
