bda6307c22d9ee14838884b9185bafdb5a2c5ba1
ammerss <amyss0907@gmail.com>
2024-03-17 22:14:56 -0400

    feat: add user dto swagger ui

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class LogInDto {
+  @ApiProperty()
   email: string;
+  @ApiProperty()
   password: string;
 }
--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,5 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty()
   name: string;
+  @ApiProperty()
   email: string;
+  @ApiProperty()
   password: string;
 }
