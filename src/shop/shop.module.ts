48d49042a79d719800824f4d5909d87ce3412e4f
redjen8 <seokwoo2525@gmail.com>
2024-03-31 20:27:16 +0900

    Feat: Order 엔티티 import를 위한 모듈 수정

--- src/shop/shop.module.ts
+++ src/shop/shop.module.ts
@@ -3,9 +3,10 @@ import { ShopController } from './shop.controller';
 import { ShopService } from './shop.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Shop } from './shop.entity';
+import { Order } from './order.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Shop])],
+  imports: [TypeOrmModule.forFeature([Shop, Order])],
   controllers: [ShopController],
   providers: [ShopService],
 })
