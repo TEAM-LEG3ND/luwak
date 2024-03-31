6a6db71e3f628251568d7e37ea3421e8f1669ec5
redjen8 <seokwoo2525@gmail.com>
2024-03-31 20:27:49 +0900

    Feat: 주문 추가 api 추가

--- src/shop/shop.controller.ts
+++ src/shop/shop.controller.ts
@@ -3,6 +3,7 @@ import { ShopService } from './shop.service';
 import { Shop } from './shop.entity';
 import { Ingredient } from './ingredient.entity';
 import { IngredientDto } from './dto/ingredient.dto';
+import { ApiBody } from '@nestjs/swagger';
 
 @Controller('shop')
 export class ShopController {
@@ -18,8 +19,15 @@ export class ShopController {
     return this.shopService.getIngredientsByShop(shopId);
   }
 
+  @ApiBody({ type: [IngredientDto] })
   @Post('/:shopId/ingredients')
   createIngredients(@Param(':shopId') shopId: number, @Body() ingredientDtos: IngredientDto[]): Promise<Shop> {
     return this.shopService.addIngredients(shopId, ingredientDtos);
+  }
+
+  @ApiBody({ type: [IngredientDto] })
+  @Post('/:shopId/order')
+  createOrder(@Param(':shopId') shopId: number, @Body() ingerdientDtos: IngredientDto[]) {
+    return this.shopService.createOrder(shopId, ingerdientDtos);
   }
 }
