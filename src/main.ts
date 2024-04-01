5cf6bea2f21670963257a88453692c771c89db41
selgyun <kindkiz@naver.com>
2024-04-01 15:10:07 +0900

    feat: Add class-validation & payment feature

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
-import {NestFactory} from '@nestjs/core';
-import {initSwaggerConfig} from './config/swagger-config';
-import {AppModule} from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { initSwaggerConfig } from './config/swagger-config';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   initSwaggerConfig(app);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      skipMissingProperties: true,
+    })
+  );
   app.enableCors({
     origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,4 +1,4 @@
-import {Injectable, Logger, NotFoundException} from '@nestjs/common';
+import {HttpException, Injectable, Logger, NotFoundException} from '@nestjs/common';
 import {HttpService} from '@nestjs/axios';
 import {Payment} from './payment.entity';
 import {Repository} from 'typeorm';
@@ -7,6 +7,7 @@ import {CancelCreateDto} from './Dto/cancel.dto';
 import {Cancel} from './cancel.entity';
 import {firstValueFrom} from 'rxjs';
 import {ConfirmPaymentDto} from './Dto/payment.dto';
+import { Order } from 'src/shop/order.entity';
 
 @Injectable()
 export class PaymentService {
@@ -15,15 +16,40 @@ export class PaymentService {
     private readonly paymentRepository: Repository<Payment>,
     @InjectRepository(Cancel)
     private readonly cancelRepository: Repository<Cancel>,
+    @InjectRepository(Order)
+    private readonly orderRepository: Repository<Order>,
     private httpService: HttpService,
-  ) {
-  }
+  ) {}
 
   private readonly logger = new Logger(PaymentService.name)
 
   async confirmPayment(data: ConfirmPaymentDto) {
     const {paymentKey, orderId, amount} = data;
     this.logger.warn(JSON.stringify(data));
+
+    // 형식 판별
+    if (isNaN(orderId) || isNaN(amount)) {
+      throw new HttpException(`[Payment Error] orderId or amount is NaN + ${orderId} + ${amount}`, 400);
+    }
+
+    // 서버 금액 검증
+    const order = await this.orderRepository.findOne({
+      where: { id: orderId }
+    });
+
+    if (!order) {
+      throw new HttpException('[Payment Error] order data not found', 404);
+    }
+
+    if (order.priceSum !== BigInt(amount)) {
+      throw new HttpException('[Payment Error] payment amount validation fail', 400);
+    }
+
+    // toss payment 요청
+    const newPayment = new Payment();
+    newPayment.paymentKey = paymentKey;
+    newPayment.orderId = orderId;
+    newPayment.totalAmount = amount;
 
     const widgetSecretKey = process.env.TOSSPAYMENTS_SECRET;
     const encryptedSecretKey = 'Basic ' + Buffer.from(widgetSecretKey + ':').toString('base64');
@@ -44,6 +70,10 @@ export class PaymentService {
       ),
     )
       .then((res) => {
+        newPayment.type = res.data['type'];
+        newPayment.cancels = res.data['cancels'];
+        
+        this.paymentRepository.save(newPayment);
         return res.data;
       })
       .catch((err) => {
@@ -62,7 +92,7 @@ export class PaymentService {
     return payment;
   }
 
-  async findByOrderId(orderId: string): Promise<Payment> {
+  async findByOrderId(orderId: number): Promise<Payment> {
     const payment = await this.paymentRepository.findOne({where: {orderId}});
 
     if (!payment) {
