1d90ccce215469cb453547495ee3bcbdcd63c471
redjen8 <seokwoo2525@gmail.com>
2024-05-06 18:25:14 +0900

    Feat: 문서 보강

--- src/shop/dto/order.dto.ts
+++ src/shop/dto/order.dto.ts
@@ -2,18 +2,19 @@ import { ApiProperty } from '@nestjs/swagger';
 import { OrderType } from 'src/common/domain/order-type';
 import { Ingredient } from '../entity/ingredient.entity';
 import { Order } from '../entity/order.entity';
+import { Type } from 'class-transformer';
 
 export class OrderDto {
   @ApiProperty()
   orderId: number;
 
-  @ApiProperty({ type: 'enum', enum: OrderType, example: 'TO_GO' })
+  @ApiProperty({ type: 'enum', enum: OrderType, example: 'TO_GO', description: '주문 타입' })
   type: OrderType;
 
-  @ApiProperty()
+  @ApiProperty({ type: BigInt, description: '주문 총 금액' })
   priceSum: BigInt;
 
-  @ApiProperty()
+  @ApiProperty({ type: Ingredient, description: '주문에 포함된 재료 목록' })
   ingredients: Ingredient[];
 
   static fromEntity(entity: Order): OrderDto {
