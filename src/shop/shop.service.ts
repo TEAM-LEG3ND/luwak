4ba6e9771c961469ba6fc1f40eccec98068374e5
redjen8 <seokwoo2525@gmail.com>
2024-05-06 17:42:29 +0900

    Feat: 주문 생성 시 타입도 저장하도록 변경

--- src/shop/shop.service.ts
+++ src/shop/shop.service.ts
@@ -1,12 +1,13 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Shop } from './shop.entity';
-import { Ingredient } from './ingredient.entity';
+import { Ingredient } from './entity/ingredient.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { IngredientDto } from './dto/ingredient.dto';
 import { randomUUID } from 'crypto';
-import { Order } from './order.entity';
+import { Order } from './entity/order.entity';
 import { OrderDto } from './dto/order.dto';
+import { OrderType } from 'src/common/domain/order-type';
 
 @Injectable()
 export class ShopService {
@@ -56,7 +57,7 @@ export class ShopService {
     return this.shopRepository.save(shop);
   }
 
-  async createOrder(shopId: number, ingredients: string[]): Promise<OrderDto> {
+  async createOrder(shopId: number, ingredients: string[], type: OrderType): Promise<OrderDto> {
     const shop = await this.shopRepository.findOne({
       where: {
         id: shopId,
@@ -77,6 +78,7 @@ export class ShopService {
     await this.orderRepository.save(newOrder);
     return {
       orderId: newOrder.id,
+      type: type,
     };
   }
 
