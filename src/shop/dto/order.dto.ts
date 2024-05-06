3c1433752f9b44d20163b43a25c2272ecd6a5930
redjen8 <seokwoo2525@gmail.com>
2024-05-06 18:07:51 +0900

    Feat: 주문 dto에 엔티티 멤버 추가 및 정적 dto 매핑 메서드 추가

--- src/shop/dto/order.dto.ts
+++ src/shop/dto/order.dto.ts
@@ -1,5 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { OrderType } from 'src/common/domain/order-type';
+import { Ingredient } from '../entity/ingredient.entity';
+import { Order } from '../entity/order.entity';
 
 export class OrderDto {
   @ApiProperty()
@@ -7,4 +9,19 @@ export class OrderDto {
 
   @ApiProperty({ type: 'enum', enum: OrderType, example: 'TO_GO' })
   type: OrderType;
+
+  @ApiProperty()
+  priceSum: BigInt;
+
+  @ApiProperty()
+  ingredients: Ingredient[];
+
+  static fromEntity(entity: Order): OrderDto {
+    const dto = new OrderDto();
+    dto.orderId = entity.id;
+    dto.type = entity.type;
+    dto.priceSum = entity.priceSum;
+    dto.ingredients = entity.ingredients;
+    return dto;
+  }
 }
