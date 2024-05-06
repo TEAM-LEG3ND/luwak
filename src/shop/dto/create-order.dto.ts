c0024c23e53500b1e759f8a4b63e4af1aeb2d886
redjen8 <seokwoo2525@gmail.com>
2024-05-06 17:41:57 +0900

    Feat: 주문 생성 및 주문 dto에 주문 타입 추가

--- src/shop/dto/create-order.dto.ts
+++ src/shop/dto/create-order.dto.ts
@@ -1,9 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { OrderType } from 'src/common/domain/order-type';
 
-export class OrderDto {
+export class CreateOrderDto {
   @ApiProperty()
-  orderId: number;
+  ingredientIds: string[];
 
   @ApiProperty({ type: 'enum', enum: OrderType, example: 'TO_GO' })
   type: OrderType;
