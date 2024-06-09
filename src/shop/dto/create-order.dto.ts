8873bc35b067b56366a70ca552cff33ff0c0e547
redjen8 <seokwoo2525@gmail.com>
2024-06-09 15:57:28 +0900

    Fix: enum 타입 transform 거치지 않아 undefined로 전달되는 이슈 해결

--- src/shop/dto/create-order.dto.ts
+++ src/shop/dto/create-order.dto.ts
@@ -1,18 +1,23 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsEnum } from 'class-validator';
 import { PackageType } from 'src/common/domain/package-type';
 import { SizeType } from 'src/common/domain/size-type';
 import { TemperatureType } from 'src/common/domain/temperature-type';
 
 export class CreateOrderDto {
   @ApiProperty()
+  @IsArray()
   ingredientIds: string[];
 
   @ApiProperty({ type: 'enum', enum: PackageType, example: 'TO_GO' })
+  @IsEnum(PackageType)
   packageType: PackageType;
 
   @ApiProperty({ type: 'enum', enum: TemperatureType, example: 'ICE' })
+  @IsEnum(TemperatureType)
   temperatureType: TemperatureType;
 
   @ApiProperty({ type: 'enum', enum: SizeType, example: 'SMALL' })
+  @IsEnum(SizeType)
   sizeType: SizeType;
 }
